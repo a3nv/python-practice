18fa21f2a71e44d2e637eecc213411a4f4308e6e
Ivan Asonov <asonovivan88@gmail.com>
2020-03-16 09:52:11 +0100

    - organize structure
    Jira:
    https://gym1q.atlassian.net/browse/CDX-1

---
+++ src/dailyinterviewpro/search/buysell/buy_sell.py
@@ -0,0 +1,11 @@
+def buy_sell(arr):
+    best_profit, current_max = 0, 0
+    for price in arr[::-1]:
+        current_max = max(current_max, price)
+        profit = current_max - price
+        best_profit = max(profit, best_profit)
+    return best_profit
+
+
+if __name__ == "__main__":
+    print(buy_sell([9, 11, 8, 5, 7, 10])) #5
